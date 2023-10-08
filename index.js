853ca977a9d8900b80c5b065b8b2d185fbe8e0f7
Panitnun-6243 <panitnun.6243@gmail.com>
2023-10-08 20:20:03 +0700

    feat: add GET product

--- index.js
+++ index.js
@@ -2,17 +2,30 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-// Middleware for logging
+// middleware for logging
 app.use((req, res, next) => {
   console.log(`${req.method} request for ${req.url}`);
   next();
 });
 
-// Middleware for parsing JSON
+// middleware for parsing JSON
 app.use(express.json());
 
+const products = [
+  { id: 1, name: "Laptop", category: "Electronics", price: 1000, stock: 5 },
+  { id: 2, name: "Phone", category: "Electronics", price: 500, stock: 10 },
+];
+
+// GET all products
 app.get("/products", (req, res) => {
-  res.send("List of products");
+  res.json(products);
+});
+
+// GET a single product by ID
+app.get("/products/:id", (req, res) => {
+  const product = products.find((p) => p.id === parseInt(req.params.id));
+  if (!product) return res.status(404).send("Product not found");
+  res.json(product);
 });
 
 app.listen(port, () => {
