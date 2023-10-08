881507d6436a0e7cfe8b59bd18f569b7787e627a
Panitnun-6243 <panitnun.6243@gmail.com>
2023-10-08 20:11:11 +0700

    feat: add middleware

--- index.js
+++ index.js
@@ -2,8 +2,17 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+// Middleware for logging
+app.use((req, res, next) => {
+  console.log(`${req.method} request for ${req.url}`);
+  next();
+});
+
+// Middleware for parsing JSON
+app.use(express.json());
+
+app.get("/products", (req, res) => {
+  res.send("List of products");
 });
 
 app.listen(port, () => {
