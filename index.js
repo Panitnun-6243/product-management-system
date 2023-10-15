fe8231a9233cd345b9e767bf079c55414a1621f3
Panitnun-6243 <panitnun.6243@gmail.com>
2023-10-15 20:56:49 +0700

    feat: add try-catch and change callback to async-await

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-const e = require("express");
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 const port = 3000;
 
 // connect MongoDB
-mongoose.connect("mongodb://localhost:27017/productDB", {
+const MONGO_URI =
+  process.env.MONGO_URI || "mongodb://localhost:27017/productDB";
+
+mongoose.connect(MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
@@ -29,65 +31,66 @@ app.use((req, res, next) => {
 // middleware for parsing JSON
 app.use(express.json());
 
-const products = [
-  { id: 1, name: "Laptop", category: "Electronics", price: 1000, stock: 5 },
-  { id: 2, name: "Phone", category: "Electronics", price: 500, stock: 10 },
-];
-
 // GET all products
-app.get("/products", (req, res) => {
-  Product.find((err, products) => {
-    if (err) {
-      return res.status(500).json({ error: err });
-    }
-    return res.json(products);
-  });
+app.get("/products", async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.json(products);
+  } catch (err) {
+    res.status(500).json({ error: err });
+  }
 });
 
 // GET a single product by ID
-app.get("/products/:id", (req, res) => {
-  Product.findById(req.params.id, (err, product) => {
-    if (err) {
-      return res.status(500).json({ error: err });
+app.get("/products/:id", async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    if (!product) {
+      return res.status(404).json({ error: "Product not found" });
     }
-    return res.json(product);
-  });
+    res.json(product);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
 
 //Post a new product
-app.post("/products", (req, res) => {
-  const newProduct = new Product(req.body);
-  newProduct.save((err) => {
-    if (err) {
-      return res.status(500).json({ error: err });
-    }
-    return res.status(201).json({ success: "Create product successfully" });
-  });
+app.post("/products", async (req, res) => {
+  try {
+    const newProduct = new Product(req.body);
+    await newProduct.save();
+    res.status(201).json({ success: "Create product successfully" });
+  } catch (err) {
+    res.status(500).json({ error: err });
+  }
 });
 
 // Put an existing product
-app.put("/products/:id", (req, res) => {
-  Product.findByIdAndUpdate(
-    req.params.id,
-    req.body,
-    { new: true },
-    (err, product) => {
-      if (err) {
-        return res.status(500).json({ error: err });
-      }
-      return res.json(product);
+app.put("/products/:id", async (req, res) => {
+  try {
+    const product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    if (!product) {
+      return res.status(404).json({ error: "Product not found" });
     }
-  );
+    return res.json(product);
+  } catch (err) {
+    return res.status(500).json({ error: err.message });
+  }
 });
 
 // Delete an existing product
-app.delete("/products/:id", (req, res) => {
-  Product.findByIdAndDelete(req.params.id, (err, product) => {
-    if (err) {
-      return res.status(500).json({ error: err });
+app.delete("/products/:id", async (req, res) => {
+  try {
+    const product = await Product.findByIdAndDelete(req.params.id);
+    if (!product) {
+      return res.status(404).json({ error: "Product not found" });
     }
     return res.json({ success: "Delete product successfully" });
-  });
+  } catch (err) {
+    return res.status(500).json({ error: err.message });
+  }
 });
 
 app.listen(port, () => {
